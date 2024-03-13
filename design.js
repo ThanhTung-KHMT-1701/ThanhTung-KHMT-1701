e3e8c128fbf9186dc956dec7f0f3f205721f9fe5
ThanhTung-KHMT-1701 <157544846+ThanhTung-KHMT-1701@users.noreply.github.com>
2024-03-13 09:35:24 +0700

    Add files via upload
    Cập nhật phần lõi cho game-02. Author: ThanhTung.

---
+++ design.js
@@ -0,0 +1,30 @@
+// let TargetElement_01 = document.querySelector(`div[data-alias="L_02-item"][data-id="1"]`)
+// let TargetElement_02 = document.querySelector(`div[data-alias="L_02-item"][data-id="3"]`)
+//
+// 	TargetElement_01.animate
+// 	(
+// 		[
+// 			{backgroundColor: "orange", transform: "scale(1.5)"},
+// 			{backgroundColor: "green", transform: "scale(1)"}
+// 		],
+// 		{
+// 			duration: 3000,
+// 			iterations: Infinity,
+// 			easing: "ease-in-out",
+// 			direction: "alternate-reverse"
+// 		}
+// 	)
+//
+// 	TargetElement_02.animate
+// 	(
+// 		[
+// 			{backgroundColor: "orange", transform: "scale(1.5)"},
+// 			{backgroundColor: "green", transform: "scale(1)"}
+// 		],
+// 		{
+// 			duration: 3000,
+// 			iterations: Infinity,
+// 			easing: "ease-in-out",
+// 			direction: "alternate-reverse"
+// 		}
+// 	)
